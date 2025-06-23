d771622402a1a47f820b0db2c62b5f02ec182289
Mariel <marielvertz@gmail.com>
2025-06-22 21:10:06 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const nombre = 'Mariel';
+const apellido = 'Ricardo';
+const nombre_Completo = (nombre + ' ' +apellido);mari rvtao EventTargetya vas ya qe s ferme ami mama temgo queueMicrotaskhacer mi ttina porque si no voy 
+a emnfddiv 
+mans y ndnaj
+
+console.log(nombre_Completo);
